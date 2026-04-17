742069398fc64d310d68cc000e3be64d6286a12d
bvanbeynum <maildrop444@gmail.com>
2026-04-17 16:15:47 -0400

    feat(arduino): implement sensor reading logic
    - initialized DHT22 and Reed switch
    
    - read temperature, humidity, light, and door state in main loop

--- arduino/office-command/office-command.ino
+++ arduino/office-command/office-command.ino
@@ -3,3 +3,18 @@
 const int DHT_PIN = 2;
 const int LIGHT_PIN = A0;
 const int REED_PIN = 3;
+
+#define DHTTYPE DHT22
+DHT dht(DHT_PIN, DHTTYPE);
+
+void setup() {
+  dht.begin();
+  pinMode(REED_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  float temperature = dht.readTemperature(true);
+  float humidity = dht.readHumidity();
+  int lightLevel = analogRead(LIGHT_PIN);
+  int doorState = digitalRead(REED_PIN);
+}
