8bab3ee53cfb47b860d62c887f3d9d4f734e608d
bvanbeynum <maildrop444@gmail.com>
2026-04-17 16:48:34 -0400

    feat(arduino): add serial transmission and watchdog timer
    - configured Serial output and WDT protection
    
    - implemented delay limit loop

--- arduino/office-command/office-command.ino
+++ arduino/office-command/office-command.ino
@@ -1,4 +1,5 @@
 #include <DHT.h>
+#include <avr/wdt.h>
 
 const int DHT_PIN = 2;
 const int LIGHT_PIN = A0;
@@ -8,11 +9,15 @@ const int REED_PIN = 3;
 DHT dht(DHT_PIN, DHTTYPE);
 
 void setup() {
+  Serial.begin(9600);
   dht.begin();
   pinMode(REED_PIN, INPUT_PULLUP);
+  wdt_enable(WDTO_4S);
 }
 
 void loop() {
+  wdt_reset();
+
   float temperature = dht.readTemperature(true);
   float humidity = dht.readHumidity();
   int lightLevel = analogRead(LIGHT_PIN);
@@ -24,4 +29,7 @@ void loop() {
   payload += "\"light\":" + String(lightLevel) + ",";
   payload += "\"door_open\":" + String(doorState == HIGH ? "true" : "false");
   payload += "}";
+
+  Serial.println(payload);
+  delay(2000);
 }
